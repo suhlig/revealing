6591a27e252d4e335d51c18e64cb08f3237d37d9
Steffen Uhlig <steffen@familie-uhlig.net>
2019-04-16 21:42:16 +0200

    Add test for updated source file

--- spec/system/target_spec.rb
+++ spec/system/target_spec.rb
@@ -5,6 +5,7 @@ require 'helpers/image'
 
 describe 'calling rake', type: 'aruba' do
   let(:project_directory) { Pathname(aruba.config.working_directory) }
+  let(:target_file) { project_directory / 'public_html/index.html' }
 
   before do
     run_command "#{aruba.root_directory}/exe/revealing init"
@@ -14,6 +15,34 @@ describe 'calling rake', type: 'aruba' do
   it 'creates the target file' do
     run_command 'bundle exec rake'
     expect(last_command_started).to be_successfully_executed
-    expect(project_directory / 'public_html/index.html').to be_file
+    expect(target_file).to be_file
+  end
+
+  context 'updating the source' do
+    let(:source_file) { project_directory / 'src/index.markdown' }
+
+    before do
+      expect(source_file.read).not_to include('appended slide')
+      source_file.open('a') { |f| f.write('# appended slide') }
+    end
+
+    it 'updates the target file' do
+      run_command 'bundle exec rake'
+      expect(last_command_started).to be_successfully_executed
+      expect(target_file.read).to include('appended slide')
+    end
+  end
+
+  context 'running it twice' do
+    before do
+      run_command 'bundle exec rake'
+      expect(last_command_started).to be_successfully_executed
+      expect(last_command_started.stderr).to_not be_empty
+    end
+
+    it 'does not trigger execution again' do
+      run_command 'bundle exec rake'
+      expect(last_command_started.stderr).to be_empty
+    end
   end
 end
